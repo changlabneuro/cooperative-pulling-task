4a0d8c966a656e1657b136ee30de3ec437de27e0
nfagan <nick@fagan.org>
2022-08-01 11:15:21 -0400

    add random functions

---
+++ src/common/random.cpp
@@ -0,0 +1,42 @@
+#include "random.hpp"
+#include <random>
+
+namespace om {
+
+class Random {
+public:
+  Random();
+  double operator()();
+  void set_seed(uint32_t seed);
+
+private:
+  std::random_device device;
+  std::mt19937 generator;
+  std::uniform_real_distribution<> distribution;
+};
+
+thread_local Random global_random_stream;
+
+Random::Random() :
+  generator(device()),
+  distribution(0.0, 1.0) {
+  //
+}
+
+double Random::operator()() {
+  return distribution(generator);
+}
+
+void Random::set_seed(uint32_t seed) {
+  generator.seed(seed);
+}
+
+double urand() {
+  return global_random_stream();
+}
+
+void seed_urand(unsigned int seed) {
+  global_random_stream.set_seed(seed);
+}
+
+}
